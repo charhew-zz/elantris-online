7c2a0f473618f3687ae71855967bf8225e01e0c5
Charlene Hew <charhew@users.noreply.github.com>
2017-07-24 21:12:42 -0700

    added nav bar

---
+++ snippets/nav.php
@@ -0,0 +1,8 @@
+<nav id="primary-nav">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="map.php">World Map</li>
+		<li><a href="#">Login</li>
+		<li><a href="#">Sign Up</li>
+	</ul>
+</nav>
